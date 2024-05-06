7dd213d14ad39c63c154d05e49cbf185ad8548af
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2024-05-06 14:45:35 +0100

    Renamed "photosorter" module to "main"

--- _photosorter.bat
+++ _photosorter.bat
@@ -1,6 +1,6 @@
 rem https://superuser.com/questions/1079403/how-to-run-multiple-commands-one-after-another-in-cmd/1079420
 
 call activate_venv.bat
-call python photosorter.py
+call python main.py
 call echo Done
 call cmd.exe
